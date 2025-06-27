bc0250b605a50da4fb0d526b7b90a2ebd6dbb29d
Divyanshu Bhardwaj <divyanshu.bhardwaj@divyanshuvrs27.successivetech.com>
2025-06-27 10:03:46 +0530

    Added questions

--- react-training/src/app/Day3/Question3/page.js
+++ react-training/src/app/Day3/Question3/page.js
@@ -1,3 +1,9 @@
+// 3.creat pages: Home and About.
+// Implement file routing.
+// Create navigation links to switch between the Home and About pages.
+// Display appropriate content on each page.
+// Add a "404 Not Found" page for any invalid routes.
+
 import { Home } from "@/app/Day3/Question3/Home/page";
 import Link from "next/link";
 
--- react-training/src/app/Day3/Question4/page.js
+++ react-training/src/app/Day3/Question4/page.js
@@ -1,3 +1,10 @@
+// 4.Enhance the previous application by adding protected pages that require authentication.
+// Create a login page with a form that accepts a username and password.
+// Implement authentication logic (e.g., hardcode a username and password for now).
+// Secure the About page so that it can only be accessed by authenticated users.
+// Redirect unauthenticated users to the login page.
+// Display a message on the Home page welcoming the authenticated user.
+
 
 import { LoginComponent } from "@/Components/Day3Components/LoginComponent";
 import {  ValidationProvider } from "@/Components/Day3Components/ValidationContext";
